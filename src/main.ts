f48360e7b3b5c23d01cf8f15b10fdf70ba36d1b1
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2023-08-19 01:42:31 +0900

    feat: 加減算のできるコンパイラの作成

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,53 @@
 import fs from "fs";
 
-function generateAssemblyCode(value: string): string {
-  return `.intel_syntax noprefix
+function generateAssemblyCode(expression: string): string {
+  const terms: string[] = [];
+  const operators: string[] = [];
+  let currentTerm = "";
+
+  for (let i = 0; i < expression.length; i++) {
+    if (expression[i] === "+" || expression[i] === "-") {
+      terms.push(currentTerm);
+      operators.push(expression[i]);
+      currentTerm = "";
+    } else {
+      currentTerm += expression[i];
+    }
+  }
+  terms.push(currentTerm);
+
+  let assemblyCode = `.intel_syntax noprefix
 .globl main
 main:
-  mov rax, ${value}
-  ret
+  mov rax, ${parseInt(terms[0])}\n`;
+
+  for (let i = 0; i < operators.length; i++) {
+    const termValue = parseInt(terms[i + 1]);
+    const operator = operators[i];
+
+    if (operator === "+") {
+      assemblyCode += `  add rax, ${termValue}\n`;
+    } else if (operator === "-") {
+      assemblyCode += `  sub rax, ${termValue}\n`;
+    }
+  }
+
+  assemblyCode += `  ret
 `;
+
+  return assemblyCode;
 }
 
 function main() {
-  const value = process.argv[2];
-  const assemblyCode = generateAssemblyCode(value);
-  const fileName = "tmp.s"; // 出力するファイル名
+  const expression = process.argv[2];
+
+  if (!expression) {
+    console.error("式を引数として指定してください");
+    return;
+  }
+
+  const assemblyCode = generateAssemblyCode(expression);
+  const fileName = "tmp.s";
 
   fs.writeFileSync(fileName, assemblyCode);
 
